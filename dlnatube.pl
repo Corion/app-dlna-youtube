015601e3aa4cb64bde37817d243fbe4c43df7a62
Max Maischein <corion@corion.net>
2014-05-14 20:35:40 +0200

    Pass files or URLs on the command line

--- dlnatube.pl
+++ dlnatube.pl
@@ -5,14 +5,27 @@ use Data::Dumper;
 
 use AnyEvent;
 use AnyEvent::DLNAServer::MediaServer;
-#use AnyEvent::HTTP;
+use JWZ::YouTubeDown;
 use File::Spec;
 
 my $media= AnyEvent::DLNAServer::MediaServer->new();
+my $info;
 #my $info= $media->add_file('//aliens/media/movies/Tim.Burtons.Vincent.avi');
-my $info= $media->add_file(File::Spec->rel2abs('Calvin Harris - Feel So Close (2011).mp4'));
+#$info= $media->add_file(File::Spec->rel2abs('Calvin Harris - Feel So Close (2011).mp4'));
 
-warn Dumper $info;
+if( @ARGV ) {
+    my $resource= $ARGV[0];
+    
+    if( -f $resource) {
+        $info= $media->add_file($resource, expires => 180);
+    } else {
+        # must be an url
+        my $yt_info= JWZ::YouTubeDown::fetch_url_info($ARGV[0]);
+        $info= $media->add_url($yt_info->{url}, expires => 180);
+    };
+} else {
+    $info= $media->add_file(File::Spec->rel2abs('Calvin Harris - Feel So Close (2011).mp4'));
+};
 
 my $url= $info->{dlna_url};
 warn "Playing $url";
@@ -20,6 +33,7 @@ system("start perl -w dlna-play-url.pl $url");
 #system(qq{start "Test" "$url"});
 
 warn "Waiting for things to happen";
-my $done= AnyEvent->condvar;
-my $timeout= AnyEvent->timer( after => 120, cb => $done );
-$done->recv;
+#my $done= AnyEvent->condvar;
+#my $timeout= AnyEvent->timer( after => 120, cb => $done );
+#$done->recv;
+AnyEvent->condvar->recv;
